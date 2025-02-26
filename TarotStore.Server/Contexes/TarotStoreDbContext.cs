caae9c93d25a1e237bbbee56a5969049bf7afc82
Alex Franchuk <alexx.fr.1994@gmail.com>
2025-02-26 15:42:01 +0100

    Add project files.

---
+++ TarotStore.Server/Contexes/TarotStoreDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TarotStore.Server.Entities;
+
+namespace TarotStore.Server.Contexes
+{
+    public class TarotStoreDbContext : DbContext
+    {
+        public TarotStoreDbContext(DbContextOptions<TarotStoreDbContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ TarotStore.Server/Entities/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace TarotStore.Server.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public float Price { get; set; }
+    }
+}
